cf92a53253bce0e9c5dc5aa208737095c018e1df
parkminho <parkmino89@gmail.com>
2026-03-06 14:21:52 +0900

    헤더 푸터 수정

--- second_design/js/T3_2nd_common.js
+++ second_design/js/T3_2nd_common.js
@@ -1,38 +1,53 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector(".header");
 
     // 헤더가 없을 경우
-    if(!header){
+    if (!header) {
         return;
     };
 
     // 로그인이 성공했을 때 헤더 ui 바꿈
-    if(localStorage.getItem("login") === "true"){
+    if (localStorage.getItem("login") === "true") {
         header.classList.add("is-login");
-    }; 
+    };
 
     const userMenu = document.querySelector(".user-menu");
     const avatarBtn = document.querySelector(".user-avatar-btn");
 
-    if(!userMenu || !avatarBtn) return;
+    if (!userMenu || !avatarBtn) return;
 
-    avatarBtn.addEventListener('click', (e)=>{
+    avatarBtn.addEventListener('click', (e) => {
         e.stopPropagation(); // 이벤트 전파 방지
         userMenu.classList.toggle("is-open");
     });
 
     document.addEventListener("click", (e) => {
-        if(!userMenu.contains(e.target)){
+        if (!userMenu.contains(e.target)) {
             userMenu.classList.remove("is-open");
         }
     });
 
     const logout = document.querySelector(".logout");
-    
-    logout.addEventListener("click", ()=>{
-    
+
+    logout.addEventListener("click", () => {
+
         localStorage.removeItem("login");
         location.replace("T3_2nd_index.html");
-    
+
     })
+
+    // 준비안된 메뉴들 전부 alert
+    const yet = document.querySelectorAll(".alert")
+
+    yet.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.stopPropagation();
+            alert("준비중입니다.");
+        })
+    })   
 });
+
+
+
+
+
